f36ed6a0ad9abc4792008165aeee19377456eb73
neeladri22 <neeladri22@gmail.com>
2022-08-06 18:25:34 +0530

    Display All Employee Payroll Details from JSON Object

--- HomePage.js
+++ HomePage.js
@@ -142,7 +142,7 @@ const createEmployeePayrollJSON = () => {
 
 
 //UC 5 – Display Employee Details from JSON Object including the Department
-
+/*
      const createInnerHtml = () => {
         const headerHtml = "<th></th><th>Name</th><th>Gender</th><th>Department</th>" +
             "<th>Salary</th><th>Start Date</th><th>Actions</th>";
@@ -172,3 +172,40 @@ const createEmployeePayrollJSON = () => {
      }
      return deptHtml;
  }
+ */
+
+ //UC 5 – Display All Employee Payroll Details from JSON Object
+
+const createInnerHtml = () => {
+    const headerHtml = "<th></th><th>Name</th><th>Gender</th><th>Department</th>" +
+        "<th>Salary</th><th>Start Date</th><th>Actions</th>";
+    let innerHtml = `${headerHtml}`;
+    let empPayrollList = createEmployeePayrollJSON();
+
+    for (const empPayrollData of empPayrollList) {
+        innerHtml = `${innerHtml}
+        
+        <tr>
+           <td><img class="profile" alt="" src="${empPayrollData._profilePic}" alt=""></td>
+           <td>${empPayrollData._name}</td>
+           <td>${empPayrollData._gender}</td>
+           <td>${getDeptHtml(empPayrollData._department)}</td>
+           <td>${empPayrollData._salary}</td>
+           <td>${empPayrollData._startDate}</td>
+                <td>
+                    <img id="${empPayrollData._id}" onclick="remove(this)" alt="delete" src="delete-black-18dp.svg">
+                    <img id="${empPayrollData._id}" onclick="update(this)" alt="edit" src="create-black-18dp.svg">
+                </td>
+       </tr>`;
+    }
+    document.querySelector('#table-display').innerHTML = innerHtml;
+
+
+}
+const getDeptHtml = (deptList) => {
+    let deptHtml = '';
+    for (const dept of deptList) {
+        deptHtml = `${deptHtml} <div class='dept-label'>${dept}</div>`
+    }
+    return deptHtml;
+}
