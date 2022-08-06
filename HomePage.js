1af2a1ab637c87f2eee1c30c353e0a8bebaf0bed
neeladri22 <neeladri22@gmail.com>
2022-08-06 18:18:02 +0530

    Display Employee Details in Tabular Format using Template Literals

--- HomePage.js
+++ HomePage.js
@@ -1,12 +1,13 @@
-// UC4 Display Employee Details in Tabular Format using Template Literals
 
+// UC4 Display Employee Details in Tabular Format using Template Literals
+/*
 window.addEventListener('DOMContentLoaded', (event) => {
     createInnerHtml();
 });
-
+*/
 /*UC 4 – Display Employee Details in Tabular Format using Template Literals */
 //Template literal ES6 feature
-
+/*
 const createInnerHtml = () => {
     const innerHtml =`
         <tr>
@@ -36,3 +37,38 @@ const createInnerHtml = () => {
     </tr>`;
     document.querySelector('#table-display').innerHTML = innerHtml;
 };
+*/
+
+// UC4 Display Employee Details in Tabular Format using Template Literals
+
+window.addEventListener('DOMContentLoaded', (event) => {
+    createInnerHtml();
+});
+
+/*UC 4 – Display Employee Details in Tabular Format using Template Literals */
+//Template literal ES6 feature
+
+const createInnerHtml = () => {
+    const headerHtml =  "<th></th><th>Name</th><th>Gender</th><th>Department</th>" +
+    "<th>Salary</th><th>Start Date</th><th>Actions</th>";
+
+    const innerHtml = `${headerHtml}
+    <tr>
+           <td><img class="profile" alt="" src="Ellipse -9.png" style="width: 70px;
+               height: 52px;"></td>
+           <td>Prateek Pai</td>
+           <td>Male</td>
+           <td>
+               <div class="dept-label">HR</div>
+               <div class="dept-label">Finance</div>
+           </td>
+           <td>800000</td>
+           <td>04 Nov 2021</td>
+           <td>
+               <img id="1" onclick="remove(this)" alt="delete" src="delete-black-18dp.svg">
+               <img id="1" onclick="update(this)" alt="edit" src="create-black-18dp.svg">
+           </td>
+    </tr>`;
+        
+    document.querySelector('#table-display').innerHTML = innerHtml;
+}
